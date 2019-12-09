510510deffcc1e66d76a5fb5887fe4c8ebd41749
62448 <624486877@qq.com>
2019-12-09 14:41:34 +0800

    update docs

--- maida/scrapy/statsmailer.py
+++ maida/scrapy/statsmailer.py
@@ -12,8 +12,9 @@ from datetime import timedelta
 
 class StatsMailer(object):
 
-    def __init__(self, stats, recipients, mail, project_name):
+    def __init__(self, stats, recipients, mail, project_name, x_priority):
         # 新增 project_name 字段，发送邮件时带上项目名，便于区分爬虫
+        # 新增 x_priority 字段，设置邮件优先级
         self.stats = stats
         self.recipients = recipients
         self.mail = mail
@@ -24,10 +25,11 @@ class StatsMailer(object):
         recipients = crawler.settings.getlist("STATSMAILER_RCPTS")
         # 在 scrapy.settings 加上 PROJECT_NAME 字段,用于设定项目名
         project_name = crawler.settings.get('PROJECT_NAME')
+        x_priority = crawler.settings.get('X_PRIORITY', '3')
         if not recipients:
             raise NotConfigured
         mail = MailSender.from_settings(crawler.settings)
-        o = cls(crawler.stats, recipients, mail, project_name)
+        o = cls(crawler.stats, recipients, mail, project_name, x_priority)
         crawler.signals.connect(o.spider_closed, signal=signals.spider_closed)
         return o
 
